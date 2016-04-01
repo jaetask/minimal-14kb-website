0272784bf3f32ccee0f9bca2250ccde1d6c43bef
Jae Task <me@jaetask.com>
2016-04-01 16:01:11 +0100

    Compiled CSS into html file, app = 427 bytes gzipped

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var uglifyCss = require('gulp-uglifycss');
-var cleanCSS = require('gulp-clean-css');
-var del = require('del');
+var gulp = require('gulp'),
+		cleanCSS = require('gulp-clean-css'),
+		concat = require('gulp-concat'),
+		del = require('del'),
+		fs = require('fs'),
+		replace = require('gulp-replace'),
+		uglifyCss = require('gulp-uglifycss');
 
+// compile all the css into a single file
 gulp.task('build-css', function() {
-	return gulp.src(['src/css/pocketgrid.css','src/css/app-style.css'])
+	return gulp.src(['src/css/pocketgrid.css','src/css/app.css'])
 		.pipe(uglifyCss({"uglyComments": true}))
 		.pipe(cleanCSS({compatibility: 'ie8'}))
 		.pipe(concat('compiled.css'))
 		.pipe(gulp.dest('build'));
 });
 
+gulp.task('html-compile', ['build-css'], function() {
+	return gulp.src('src/html/index.html')
+		.pipe(replace('{{CSS}}', '<style>'+fs.readFileSync('build/compiled.css', 'utf8')+'</style>'))
+		.pipe(replace('{{JS}}', '/** INJECT JS HERE! */'))
+		.pipe(gulp.dest('public'));
+});
+
 gulp.task('clean', function() {
 	return del(['build']);
 });
 
-gulp.task('default', ['clean', 'build-css']);
+gulp.task('default', ['clean','html-compile']);
